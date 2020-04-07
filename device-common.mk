8601c19fb8e1c9b253b94dededb44ad103151559
muhashh <muhashh@protonmail.com>
2020-04-07 13:31:10 +0100

    universal8895: add audio xmls from framework
    Change-Id: I6cbc4482f312319b76699cd2b7c3c981a8839018

--- device-common.mk
+++ device-common.mk
@@ -43,6 +43,13 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     libtinycompress
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/vendor/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/vendor/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/vendor/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/vendor/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml \
+
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
