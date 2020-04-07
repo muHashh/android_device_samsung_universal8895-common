01c51afd98a1b74ecc8289dca70ab022655ab870
muhashh <muhashh@protonmail.com>
2020-04-07 13:12:08 +0100

    universal8895: remove gcc toolchain
    Change-Id: I7b0f235f855adf553de2ac70d2023d377bf26a06

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,7 +69,6 @@ TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 # Kernel
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/universal8895
